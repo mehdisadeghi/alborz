1bbe26c5d43fe03aa1d8b501626c42269843ac81
Mehdi Sadeghi <mehdi@mehdix.org>
2026-09-03 09:26:06 +0200

    Stop keeping a browser session with the DAV server
    SabreDAV answers 401 "CSRF check not passed" to a request carrying a
    session cookie, whatever credentials it also sends.

--- plugins/caldav/caldav.go
+++ plugins/caldav/caldav.go
@@ -101,7 +101,6 @@ type davCollectionProps struct {
 }
 
 func (p *plugin) httpClient(session *alborz.Session) *http.Client {
-	jar := p.jar(session)
 	return &http.Client{
 		// A wedged DAV server fails the request instead of hanging it.
 		Timeout: requestTimeout,
@@ -109,8 +108,7 @@ func (p *plugin) httpClient(session *alborz.Session) *http.Client {
 			upstream: http.DefaultTransport,
 			session:  session,
 			debug:    p.debug,
-		}, jar),
-		Jar: jar,
+		}),
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
